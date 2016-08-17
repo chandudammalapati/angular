e230c847ba6d806dfbfe073b488edd6669ea4462
chandudammalapati <chandudammalapati@gmail.com>
2016-08-17 00:24:14 -0400

    adding mockup to testbranch

---
+++ eventManage/script/myCustom.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="../lib/angular/angular.js" />
+var myApp = angular.module('myModule', []);
+myApp.controller('myJsonData', ['$scope', 'retrieveJson', function($scope, retrieveJson){
+    var jsonRetrieved = retrieveJson.itemDetails();
+    jsonRetrieved.then(function (data) {
+        $scope.itemDetails = data;
+         
+    });
+    $scope.retrieveData = function (input) {
+         
+        var selectedEvent;
+        $scope.showValue = false;
+       
+        if (!input) {
+            return input;
+            $scope.showValue = false;
+        } else {
+            for (var index = 0 ; index < $scope.itemDetails.length ; index++) {
+
+                if (input == $scope.itemDetails[index].eventName) {
+                    // input = $scope.myDataEvent[index];
+                    $scope.selectedEvent = $scope.itemDetails[index];
+                    $scope.showValue = true;
+                }
+                
+                
+            }
+        }
+
+
+        }
+    
+    }]);
---
+++ eventManage/script/services.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="myCustom.js" />
+myApp.factory("retrieveJson", ['$http', function ($http) {
+    var retrieveJson = {
+        itemDetails: function () {
+            return $http(
+                {
+                    url:"data.json",
+            method: "GET",
+            })
+    .then(function(response){
+        return response.data;
+    });
+}
+};
+return retrieveJson;
+}]);
